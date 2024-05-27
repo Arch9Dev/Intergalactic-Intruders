2ab37c25d3cdb02e2194838f309f90945a12b272
Arch9Dev <bendt03@outlook.co.nz>
2024-05-28 09:48:18 +1200

    New Logo

--- Intergalactic-Intruders/main.py
+++ Intergalactic-Intruders/main.py
@@ -29,7 +29,7 @@ def main_menu():
     while MainRunning:
         # Blit the background image onto the screen
         screen.blit(constants.BACKGROUND_IMAGE, (0, 0))
-        title_rect = constants.TITLE_IMAGE.get_rect(center=(constants.SCREEN_WIDTH // 2, 150))
+        title_rect = constants.TITLE_IMAGE.get_rect(center=(constants.SCREEN_WIDTH // 2, 170))
         screen.blit(constants.TITLE_IMAGE, title_rect)
 
         # Event handling
