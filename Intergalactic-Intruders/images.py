9f91f2f9e22bacc8b0c8cdc51b962ce603fb55f1
Arch9Dev <bendt03@outlook.co.nz>
2024-05-28 09:50:08 +1200

    image

--- Intergalactic-Intruders/images.py
+++ Intergalactic-Intruders/images.py
@@ -6,9 +6,9 @@ pygame.init()
 
 # Load title image
 def load_title_image():
-    title_image_path = os.path.join("Intergalactic-Intruders", "images", "logo.png")
+    title_image_path = os.path.join("Intergalactic-Intruders", "images", "logo4.png")
     title_image = pygame.image.load(title_image_path)
-    return pygame.transform.scale(title_image, (600, 300))
+    return pygame.transform.scale(title_image, (600, 400))
 
 
 # Load tutorial image
