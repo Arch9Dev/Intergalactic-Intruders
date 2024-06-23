5fe9c0945b04b5337bb4dc5d0444e91bddcc6aed
luwiess <lewismcd27@gmail.com>
2024-06-24 10:57:09 +1200

    game win implemented

--- Intergalactic-Intruders/test.py
+++ Intergalactic-Intruders/test.py
@@ -5,6 +5,7 @@ import sounds
 import os
 import GameOver
 import constants
+import Gamewin
 from PIL import Image, ImageSequence
 
 def show_test():
@@ -626,6 +627,10 @@ def show_test():
                     if health > 0:
                         Screen.blit(barrier_image[health], (barrier[0], barrier[1]))
                 # All done with collisino 
+
+                if (len(Invader_X) <= 0):
+                    Screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+                    Gamewin.show_GameWin(Accuracy, Frames, current_time, Score_val)
 
         # now just rendering stuff every frame
         Player(Player_X, Player_Y)
