493197b25a0aa3580143b6a59739130a1e4bf59e
Aedan-Paltridge-Nicholls <aedan432@gmail.com>
2024-06-24 10:41:24 +1200

    Controls And  Gameplay

--- Intergalactic-Intruders/controls.py
+++ Intergalactic-Intruders/controls.py
@@ -1,16 +1,19 @@
 import pygame
 import constants
+import Gameplay
 
 def show_controls():
     pygame.init()
     screen = constants.screen
     pygame.display.set_caption("CONTROLS")
-  
+    Next_Button = constants.Button("NEXT",screen.get_width()/7*6,screen.get_height()/7,-90,-100,constants.Colour_Palettes["Green_Buttons"])
     Back_button = constants.BackButton(constants.Colour_Palettes["Red_Buttons"],"Settings")
-    Control_Buttons =[Back_button]
-    controls_running = True
+    Control_Buttons =[Back_button,Next_Button]
+    controls_running = True         
+    controls_image = constants.CONTROLS_IMAGE
+
     while controls_running:
-        screen.blit(constants.BACKGROUND_IMAGE, (0,0))
+        screen.blit(controls_image, (0,0))
         for event in pygame.event.get():
             if event.type == pygame.MOUSEMOTION:
                 MousePos = pygame.mouse.get_pos()
@@ -21,15 +24,15 @@ def show_controls():
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                if Next_Button.rect.collidepoint(event.pos):
+                    Gameplay.show_GAMEPLAY()
                 if Back_button.rect.collidepoint(event.pos):
                     Back_button.ReturnTo()
         for button in Control_Buttons:
             button.draw()
 
         # Load and render controls image
-        controls_image = constants.CONTROLS_IMAGE
-        controls_image_rect = controls_image.get_rect(center=(constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT // 2))
-        screen.blit(controls_image, controls_image_rect)
+       
 
         # Render back button with border
         
