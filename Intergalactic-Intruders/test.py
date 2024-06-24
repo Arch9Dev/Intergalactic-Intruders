b580bf6195c7e52d40da2b449f1e2afd34fdbc4d
Arch9Dev <bendt03@outlook.co.nz>
2024-06-25 08:56:18 +1200

    pause changed

--- Intergalactic-Intruders/test.py
+++ Intergalactic-Intruders/test.py
@@ -463,10 +463,12 @@ def show_test():
                         paused = False
                     elif settings_button.rect.collidepoint(mouse_pos) and mouse_click[0]:
                         Screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-                        main.main_menu()
+                        settings.show_settings()
+                        paused = True
                     elif main_menu_button.rect.collidepoint(mouse_pos) and mouse_click[0]:
                         Screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-                        settings.show_settings()
+                        main.main_menu()
+                        
 
             # Game logic and update stuff
             else:
